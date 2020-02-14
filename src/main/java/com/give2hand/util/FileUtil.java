64e0d9480e416a7552df51335c11de24b79ef2b3
moorthy127@gmail.com <User1@LP-5CD9176L8V.HCLT.CORP.HCL.IN>
2020-02-14 19:48:42 +0530

    changes in file util class

--- src/main/java/com/give2hand/util/FileUtil.java
+++ src/main/java/com/give2hand/util/FileUtil.java
@@ -5,18 +5,27 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class FileUtil {
-	
+
 	public byte[] getTaxCertificate(String taxUrl) throws IOException {
 		
-		File file = new File(taxUrl);
-		FileInputStream fis = new FileInputStream(file);
-		byte[] data = new byte[(int) file.length()];
-		fis.read(data);
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		data = bos.toByteArray();
-		fis.close();
-		return data;
+		try {
+			File file = new File(taxUrl);
+			FileInputStream fis = new FileInputStream(file);
+			byte[] data = new byte[(int) file.length()];
+			fis.read(data);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			data = bos.toByteArray();
+			fis.close();
+			return data;
+		}
+		catch (IOException e) {
+	       log.error("Error Occured in FileUtil...");
+	    }
+		return null;
 	}
 
 }
