b63f8ed290632dd8f639af9c976890e759996196
moorthy127@gmail.com <User1@LP-5CD9176L8V.HCLT.CORP.HCL.IN>
2020-02-14 13:36:17 +0530

    changes in donation entity  for unwanted imports

---
+++ src/main/java/com/give2hand/entity/UserDonationScheme.java
@@ -0,0 +1,47 @@
+package com.give2hand.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.give2hand.common.GiveToHandEnum.PaymentStatus;
+import com.give2hand.common.GiveToHandEnum.PaymentType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ * @author Amala.S
+ * @version V1.1
+ * @since 14-02-2020
+ *
+ */
+@Getter
+@Setter
+@Entity
+public class UserDonationScheme {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer donationId;
+	@ManyToOne
+	@JoinColumn(name = "scheme_id")
+	private DonationScheme scheme;
+	private String taxCertificateUrl;
+	private String name;
+	@Column(unique = true)
+	private Long phoneNumber;
+	private String panNumber;
+	@Enumerated(EnumType.STRING)
+	private PaymentType paymentType;
+	@Enumerated(EnumType.STRING)
+	private PaymentStatus paymentStaus;
+
+}
--- src/main/java/com/give2hand/service/SchemeService.java
+++
@@ -1,5 +0,0 @@
-package com.give2hand.service;
-
-public interface SchemeService {
-
-}
